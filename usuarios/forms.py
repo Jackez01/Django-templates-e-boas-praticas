604166e549c35d733c1c57449dc40fedd9960426
Jackez01 <lucasgefreitas@gmail.com>
2026-08-03 16:36:44 -0300

    Autenticação cadastro e login

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -20,3 +20,49 @@ class LoginForms(forms.Form):
             'placeholder': 'Digite sua senha'
         })
     )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label='Nome de Cadastro',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'EX.: João Silva'
+
+        })
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        max_length=100,
+        widget=forms.EmailInput(
+          attrs={
+            'class': 'form-control',
+            'placeholder': 'EX.: joao@gmail.com'
+            }  
+        )
+    )
+
+    senha_1=forms.CharField(
+            label='Senha',
+            required=True,
+            max_length=70,
+            widget=forms.PasswordInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Digite sua senha'
+            }
+        )
+    )
+    senha_2=forms.CharField(
+        label='Digite a senha novamente',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua senha'
+            }
+        )
+    )
